2351e03d1dd838642cde33609421e55dc7d8f8dc
GBarker <bluecoast86@gmail.com>
2017-10-26 15:47:31 -0700

    Initial commit after copy from malfunctioning online workspace

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import ListBooks from './ListBooks.js';
+import SearchBooks from './SearchBooks.js'
+import * as BooksAPI from './BooksAPI';
+import './App.css'
+
+class BooksApp extends Component {
+  state = {
+    books : [],
+    searchterms: ['Android', 'Art', 'Artificial Intelligence', 'Astronomy',
+      'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief',
+      'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics',
+      'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development',
+      'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education',
+      'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness',
+      'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo',
+       'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn',
+       'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery',
+       'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry',
+       'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics',
+       'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh',
+       'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate',
+       'Virtual Reality', 'Web Development', 'iOS']
+  }
+  componentDidMount() {
+    BooksAPI.get("74XNzF_al3MC").then((book) => {console.log(book)})
+	  BooksAPI.getAll().then((books) => {console.log(books)})
+ 	  BooksAPI.getAll().then((books) => {this.setState({ books: books })})
+	}
+
+  changeShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+ 	  BooksAPI.getAll().then((books) => {this.setState({ books: books })})
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <Route exact path="/" render={() => (
+          <ListBooks
+            books={this.state.books}
+            onChangeShelf={this.changeShelf}
+          />
+        )}/>
+
+        <Route exact path="/search" render={({ history }) => (
+          <SearchBooks
+            onChangeShelf={(book, shelf) => {
+              this.changeShelf(book, shelf)
+              history.push('/')
+            }}
+          />
+        )}/>
+      </div>
+    )
+  }
+}
+
+export default BooksApp;
---
+++ src/SearchBooks.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as BooksAPI from './BooksAPI';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+
+class SearchBooks extends Component {
+  static propTypes = {
+    searchterms: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: '',
+    searchedBooks: []
+  }
+
+  searchBooks = (query) => {
+    this.setState({ query: query.trim() })
+    BooksAPI.search(this.state.query, "20").then(books => {
+      this.setState(state => ({
+        searchedBooks: books
+      }))
+    })
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
+  clearQuery = (query) => {
+    this.setState({query: ''})
+  }
+
+  render() {
+    const { searchterms } = this.props
+    const { query, searchedBooks } = this.state
+    let showingSearchTerms
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      showingSearchTerms = searchterms.filter((searchterm) => match.test(searchterm))
+    } else {
+      showingSearchTerms = []
+    }
+
+    showingSearchTerms.sort(sortBy())
+
+    return(
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className='close-search' to='/'>Close</Link>
+          <div className="search-books-input-wrapper">
+            /*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */
+            <input
+              className='search-books'
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+
+              onChange={(e) => this.searchBooks(e.target.value)}
+              />
+              <input type="submit" className="search-books-submit-btn" value="Search"/>
+              <button onClick={this.clearQuery}>Clear</button>
+            <select>
+            {showingSearchTerms.length !== 0 && showingSearchTerms.map((searchterm) => (
+              <option className="search-term" value={searchterm}>{searchterm}</option>
+            ))}
+            </select>
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+          {searchedBooks !== [] && (searchedBooks.map((book) => (
+            <li key={searchedBook.id} className='book-list-item'>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                  <div className="book-shelf-changer">
+                    <select onChange={(e) => onChangeShelf(searchedBook, e.target.value)}>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{searchedBook.title}</div>
+                <div className="book-authors">{searchedBook.authors}</div>
+              </div>
+            </li>
+          )))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchBooks;
